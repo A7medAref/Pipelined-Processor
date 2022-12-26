5fb66508ead4725d917bd09f06d7cee4090eaf35
omarfareed <fareedomar159@gmail.com>
2022-12-26 13:59:28 +0200

    alu test bench

--- ALU_tb.v
+++ ALU_tb.v
@@ -12,6 +12,7 @@ module Testing;
         in1 = 1;
         in2 = 5;
         #50
+        controlSignal = 5;
         in1 = -1;
         in2 = -5;
         #50
@@ -21,5 +22,32 @@ module Testing;
         #50
         controlSignal = 1;
         in2 = 16'b1111111111111111;
+        #50
+        controlSignal = 2;
+        in1 = 5;
+        #50
+        controlSignal = 3;
+        in1 = 10;
+        #50
+        controlSignal = 4;
+        in1 = 100;
+        #50        
+        controlSignal = 6;
+        in1 = 3;
+        in2 = 10;
+        #50
+        in1 = 10;
+        in2 = 3;
+        #50
+        controlSignal = 7;
+        in1 = 10;
+        in2 = 5;
+        #50
+        controlSignal = 8;
+        #50;
+        controlSignal = 11;
+        #50;
+        controlSignal = 12;
+
     end
 endmodule
