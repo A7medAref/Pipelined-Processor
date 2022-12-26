e9ec51c5ae86578e457195bc2bc14944f41a5b96
omarfareed <fareedomar159@gmail.com>
2022-12-26 14:27:19 +0200

    alu final

--- ALU.v
+++ ALU.v
@@ -1,9 +1,10 @@
 module ALU#(parameter N=16) (input[N-1:0] in_src, input[N-1:0] in_dst, input[3:0] controlSignal, 
-            output[N-1:0] out, output carryFlay, output zeroFlag, output negFlag);
-    assign {carryFlay, out} = (controlSignal == 1) ? ~in_src :
+            output[N-1:0] out, output carryFlag, output zeroFlag, output negFlag);
+    wire is_alu;
+    assign {carryFlag, out} = (controlSignal == 1) ? ~in_src :
                               (controlSignal == 2) ? (in_src + 1) :
                               (controlSignal == 3) ? (in_src - 1) : 
-                              (controlSignal == 4) ? in_dst :
+                              (controlSignal == 4) ? {carryFlag , in_dst} :
                               (controlSignal == 5) ? (in_src + in_dst) :
                               (controlSignal == 6) ? (in_src - in_dst) :
                               (controlSignal == 7) ? (in_src & in_dst) :
@@ -12,10 +13,11 @@ module ALU#(parameter N=16) (input[N-1:0] in_src, input[N-1:0] in_dst, input[3:0
                               (controlSignal == 10) ? (in_src) :// TODO: need immediate value
                               (controlSignal == 11) ? {1 , out} :
                               (controlSignal == 12) ? {0 , out} :
-                              (controlSignal == 13) ? {0, in_src} : // TODO: control signal hasn't been sent
-                              (controlSignal == 14) ? {0, in_src} : {carryFlay, out}; // TODO: control signal hasn't been sent
+                              (controlSignal == 13) ? {carryFlag , in_src} : // TODO: control signal hasn't been sent
+                              (controlSignal == 14) ? {carryFlag , in_src} : {carryFlag, out}; // TODO: control signal hasn't been sent
 // 2=ldd , 3=std,==> pass in_src
-    assign zeroFlag = (controlSignal != 3) ? !(|out) ? 1 : 0 :
-                            zeroFlag;
-    assign negFlag = (controlSignal != 3) ? out[N-1] : negFlag;
+    
+    assign is_alu = !(controlSignal == 4 || controlSignal >= 13);
+    assign zeroFlag = (is_alu) ? !(|out) : zeroFlag;
+    assign negFlag = (is_alu) ? out[N-1] : negFlag;
 endmodule
